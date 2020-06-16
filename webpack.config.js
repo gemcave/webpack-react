fc3cb5f897ee95534443c8e8eb3d1befc1147383
yerowyn <yerowyn@gmail.com>
2020-06-16 13:18:14 +0600

    init

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+	entry: "./src/App.js",
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: "bundle.js",		
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+				}
+			}
+		]
+	}
+	plugins: [new HtmlWebpackPlugin({template: "./src/index.html"})]
+}
